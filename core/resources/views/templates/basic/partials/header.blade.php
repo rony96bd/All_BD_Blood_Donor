5047c8cc1e71cec4e984199e18f458e3dc574033
Md. Rakib Uddin <r.rony9966@gmail.com>
2023-09-07 17:25:01 +0600

    changes

--- core/resources/views/templates/basic/partials/header.blade.php
+++ core/resources/views/templates/basic/partials/header.blade.php
@@ -6,6 +6,8 @@
     <div class="header__top">
         <div class="container">
             <div class="row align-items-center gy-2" style="font-family: 'BenSen', sans-serif; font-size: 20px;">
+                <div class=".d-block .d-sm-none">Show only mobile</div>
+                <div class="col top-nav-bar">
                 @php
                     use Rajurayhan\Bndatetime\BnDateTimeConverter; // on Top
 
@@ -16,6 +18,10 @@
                     echo $dateConverter->getConvertedDateTime($date_now, 'BnEn', 'l, jS F Y ইং'); // শুক্রবার ৭ই সেপ্টেম্বর ২০১৮ দুপুর ১২:১৯:৫০
                     $dateConverter->getConvertedDateTime('2018-09-07 12:19:50 pm', 'EnEn', ''); // Friday 7th September 2018 12:19:50 PM
                 @endphp
+                </div>
+                <div class="col top-nav-bar adv">
+                    ---Advertisement---
+                </div>
                 {{-- <div class="col-lg-6 col-md-8 col-sm-9">
                     <ul class="header__info-list d-flex flex-wrap align-items-center justify-content-sm-start justify-content-center">
                         <li><a href="tel:{{__($contact->data_values->contact_number)}}"><i class="las la-phone"></i> {{__($contact->data_values->contact_number)}}</a></li>
