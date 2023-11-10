99fce53cd18f478629a96b68329aa1c36504fa57
Md. Rakib Uddin <r.rony9966@gmail.com>
2023-11-10 22:09:00 +0600

    add meta

--- core/resources/views/templates/basic/layouts/frontend.blade.php
+++ core/resources/views/templates/basic/layouts/frontend.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="google-site-verification" content="6pKQERTvX6kqmyMZpGXFRdPUdHMJjxzryM9Y_C1PlDs" />
     <title>{{ $general->sitename(__($pageTitle)) }}</title>
     @include('partials.seo')
     <link rel="icon" type="image/png" href="{{ getImage(imagePath()['logoIcon']['path'] . '/favicon.png') }}"
